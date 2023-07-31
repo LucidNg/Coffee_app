481aa0e4290ae7d072e29899c255bfa1a8359752
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-08-01 00:37:57 +0700

    redeme screen

--- lib/screens/redeme_screen.dart
+++ lib/screens/redeme_screen.dart
@@ -28,7 +28,7 @@ class _RedemePageState extends State<RedemePage> {
                   padding: const EdgeInsets.only(left: 30),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(right: 160),
+                  padding: EdgeInsets.only(right: 170),
                   child: Text(
                     'Redeem',
                     style:
@@ -36,6 +36,215 @@ class _RedemePageState extends State<RedemePage> {
                   ),
                 )
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 35, right: 35),
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 15),
+                width: 320,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "images/Americano.png",
+                      height: 70,
+                      width: 70,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Americano',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 5),
+                                child: Text('Valid until'),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 9),
+                      child: FilledButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Color.fromARGB(255, 56, 34, 18))),
+                          child: const Text(
+                            '200pt',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 35, right: 35),
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 15),
+                width: 320,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "images/Cappuccino.png",
+                      height: 70,
+                      width: 70,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Cappuccino',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 5),
+                                child: Text('Valid until'),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    FilledButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll<Color>(
+                                Color.fromARGB(255, 56, 34, 18))),
+                        child: const Text(
+                          '200pt',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 35, right: 35),
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 15),
+                width: 320,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "images/Flat White.png",
+                      height: 70,
+                      width: 70,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Flat White',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 5),
+                                child: Text('Valid until'),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: FilledButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Color.fromARGB(255, 56, 34, 18))),
+                          child: const Text(
+                            '200pt',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 35, right: 35),
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 15),
+                width: 320,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "images/Mocha.png",
+                      height: 70,
+                      width: 70,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, right: 20),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Mocha',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 5),
+                                child: Text('Valid until'),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 45),
+                      child: FilledButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(
+                                  Color.fromARGB(255, 56, 34, 18))),
+                          child: const Text(
+                            '200pt',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
